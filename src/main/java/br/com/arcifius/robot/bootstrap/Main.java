961b0fd946630c6758b48cfc1a7c73f767e8da30
Augusto Russo <augustopaladin@gmail.com>
2018-06-07 01:58:25 -0300

    Removed data

--- src/main/java/br/com/arcifius/robot/bootstrap/Main.java
+++ src/main/java/br/com/arcifius/robot/bootstrap/Main.java
@@ -16,8 +16,8 @@ import java.util.logging.Logger;
  */
 public class Main {
 
-    // private final static String TOKEN_DE_ACESSO_DA_PAGINA = "EAADAetkDqMIBAKaZC5zYfcCPZAN5mmhoQwsD6NpqfB5UhF9y2onXMhtpwjwSM2OCWhPvR1xqmLSBjfT702NuFf1TfyZAnPMlMeVHGziuf3AlYDNzAyVHGRS8k1C9NZCaWyZCux6CdFhzwALRc0AvNcvo0lZBqVdwV6nwRAuFcwKwZDZD";
-    // private final static String ID_DA_PAGINA = "arcifiustestes";    
+    // private final static String TOKEN_DE_ACESSO_DA_PAGINA = "";
+    // private final static String ID_DA_PAGINA = "";    
 
     public static void main(String[] args) {
         // IPublisher publisher = new FacebookPublisher(ID_DA_PAGINA, TOKEN_DE_ACESSO_DA_PAGINA);
